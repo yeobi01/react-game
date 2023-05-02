7481267127a266d69cdcb1c6dd31101206bcbefa
yeobi <yeobi_01@naver.com>
2023-05-02 17:30:04 +0900

    [study] 2.끝말잇기 끗

--- 2.끝말잇기/WordRelay.jsx
+++ 2.끝말잇기/WordRelay.jsx
@@ -1,12 +1,43 @@
 const React = require('react');
-const { Component } = React;
+const { useState, useRef } = React;
 
 const WordRelay = () => {
-    return (
+    const [word, setWord] = useState('제로초');
+    const [value, setValue] = useState('');
+    const [result, setResult] = useState('');
+    const inputRef = useRef(null);
+
+    const onSubmitForm = (e) => {
+        e.preventDefault();
+        if(word[word.length - 1] === value[0]){
+            setResult('딩동댕');
+            setWord(value);
+            setValue('');
+            inputRef.current.focus();
+        } else {
+            setResult('땡');
+            setValue('');
+            inputRef.current.focus();
+        }
+    };
+
+    const onChangeInput = (e) => {
+        setValue(e.target.value);
+    };
+
+
+    return(
         <>
-            <h1>안녕하세요</h1> 
+            <div>{word}</div>
+            <form onSubmit={onSubmitForm}>
+                <input ref={inputRef} value={value} onChange={onChangeInput} />
+                <button>입력!</button>
+            </form>
+            <div>{result}</div>
         </>
-    )
+    );
 }
+
+
 
 module.exports = WordRelay;
--- 2.끝말잇기/webpack.config.js
+++ 2.끝말잇기/webpack.config.js
@@ -1,26 +1,45 @@
 const path = require('path');
+const RefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 
 module.exports = {
     name: 'wordrelay-setting',
     mode: 'development', // 실서비스: production
     devtool: 'eval',
-    // resolve: {
-    //     extensions: ['.js', '.jsx'],
-    // },
+    resolve: {
+        extensions: ['.js', '.jsx'],
+    },
     entry: {
-        app: ['./client.jsx'],
+        app: ['./client'],
     }, // 입력
     module: {
         rules: [{
-            test: /\.jsx?/,
+            test: /\.jsx?$/,
             loader: 'babel-loader',
             options: {
-                presets: ['@babel/preset-env', '@babel/preset-react'],
+                presets: [['@babel/preset-env',{
+                    targets: {
+                        browsers: ['> 1% in KR'],
+                    },
+                }],
+                '@babel/preset-react'],
+                plugins: [
+                    '@babel/plugin-proposal-class-properties',
+                    'react-refresh/babel',
+                ],
             },
         }],
     },
+    plugins: [
+        new RefreshWebpackPlugin()
+    ],
     output: {
         path: path.join(__dirname, 'dist'),
-        filename: 'app.js'
+        filename: 'app.js',
+        publicPath: '/dist/',
     }, // 출력
+    devServer: {
+        devMiddleware: { publicPath: '/dist' },
+        static: { directory: path.resolve(__dirname) },
+        hot: true,
+    }
 };
